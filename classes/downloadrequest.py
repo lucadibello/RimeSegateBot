23821a047add0415a110789f315d16523286086d
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-06-19 09:22:25 +0100

    Update .gitignore

--- classes/downloadrequest.py
+++
@@ -1,8 +0,0 @@
-class DownloadRequest:
-    
-    def __init__(self, url, filename):
-        self.url = url
-        self.filename = filename
-
-    def get_dict():
-        return {"url": url, "filename": filename}
