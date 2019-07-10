c4d58bc1d73213e2166e42612cdec9d1f60efee5
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-07-10 12:08:33 +0100

    Full youtube-dl support

--- classes/downloadmanager.py
+++ classes/downloadmanager.py
@@ -7,15 +7,13 @@ import os
 from tqdm import tqdm
 
 '''
-This class is used for downloding content from the internet using the python native library 'urllib'.
+This class is used for downloading content from the internet using the python native library 'urllib'.
 '''
 
 
 class DownloadManager:
-    '''
-        Constructor method. It saves into an attribute the requested resource.
-    '''
 
+    # Constructor method. It saves into an attribute the requested resource.
     def __init__(self, download_req: DownloadRequest, notifier=None, session=None):
         self.download_req = download_req
         self.notifier = notifier
@@ -24,39 +22,40 @@ class DownloadManager:
         ''' Create progress bar '''
         self.bar = None
 
-    '''
-        This function is used for downloading the requested file 
-        from the internet and save it into a specific folder.
-    '''
-
-    def download_file(self, save_path, overwrite_check=False, automatic_filename=False):
+    # This function is used for downloading the requested file
+    # from the internet and save it into a specific folder.
+    def download_file(self, save_path, overwrite_check=False, automatic_filename=False, new_download_method=True):
         try:
-            if not automatic_filename:
-                ''' Create the urllib object for downloading files on the internet '''
-                filename = self.download_req.filename
+            full_path = ""
+            if new_download_method:
 
+                if not automatic_filename:
+                    ''' Create the urllib object for downloading files on the internet '''
+                    filename = self.download_req.filename
+
+                else:
+                    ''' It will save the file with a unique filename based on the computer time (date and hour) '''
+                    current_time = datetime.datetime.now()
+                    filename = current_time.strftime("%m%d%Y-%H%M%S")
+
+                ''' Normalize the name (only numbers and letters) '''
+                filename = self._normalize_filename(filename) + self._get_file_extension(self.download_req.url)
+
+                ''' Build path '''
+                full_path = os.path.join(save_path, filename)
+
+                ''' Check for overwrite if it's enabled '''
+                if overwrite_check:
+                    print("[!] Starting overwrite check")
+                    if not self.overwrite_check(save_path, filename):
+                        return "[Overwrite] Error, with this filename you will overwrite a file"
+
+                # Connects to the site and download the media
+                urllib.request.urlretrieve(self.download_req.url, full_path, self.download_progress)
             else:
-                ''' It will save the file with a unique filename based on the computer time (date and hour) '''
-                current_time = datetime.datetime.now()
-                filename = current_time.strftime("%m%d%Y-%H%M%S")
-
-            ''' Normalize the name (only numbers and letters) '''
-            filename = self._normalize_filename(filename) + self._get_file_extension(self.download_req.url)
-
-            ''' Build path '''
-            full_path = os.path.join(save_path, filename)
-
-            ''' Check for overwrite if it's enabled '''
-            if overwrite_check:
-                print("[!] Starting overwrite check")
-                if not self.overwrite_check(save_path,
-                                            filename): return "[Overwrite] Error, with this filename you will overwrite a file"
-
-            # Connects to the site and download the media
-            urllib.request.urlretrieve(self.download_req.url, full_path, self.download_progress)
+                self._download(save_path, self.download_req.url)
 
             print("[Downloader] File named {} saved correctly".format(full_path))
-
             return True
 
         except FileNotFoundError as f:
@@ -65,6 +64,24 @@ class DownloadManager:
             return "Error while saving file in {}".format(save_path)
 
     TOT_DOWNLOADED = 0
+
+    def _download(self, save_path, url):
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': save_path + "/" + '%(title)s.%(ext)s',
+            'progress_hooks': [self.download_hook],
+        }
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+
+        except youtube_dl.utils.DownloadError as err:
+            print("Error detected: " + err.exc_info)
+
+    @staticmethod
+    def download_hook(d):
+        if d["status"] == 'finished':
+            print("Finished downloading video. Now converting...")
 
     def download_progress(self, count, block_size, total_size):
 
