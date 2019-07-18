c7f9c027187a274053c3139bff43502a5b886e29
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-07-18 21:20:50 +0100

    Normalized comment using pyDoc format

--- classes/config.py
+++ classes/config.py
@@ -1,36 +1,41 @@
 import json
 
-'''
-This class is used for manage the config file
-'''
 class Config:
+    """ This class is used for manage the config file """
 
-    '''
-        Constructor method. It saves into an attribute the config file path.
-    '''
     def __init__(self, filepath: str):
+        """
+        Constructor method. It saves into an attribute the config file path.
+
+        :param filepath: Config file path
+        """
+
         self.filepath = filepath
 
-    '''
-        This private function is used for writing settings into the config
-        file.
-    '''
     def _write_json(self, data: dict):
-        with open(self.filepath, 'w+') as outfile:  
+        """
+        This private function is used for writing settings into the config file.
+
+        :param data: config dictionary to write to the config.json file.
+        """
+
+        with open(self.filepath, 'w+') as outfile:
             json.dump(data, outfile)
     
-    '''
-        This private function is used for reading all the settings stored in
-        the config file.
-    '''
     def _read_json(self) -> dict:
-        with open(self.filepath) as json_file:  
+        """
+        This private function is used for reading all the settings stored in the config file.
+
+        :return: Dictionary built as "setting_name: setting_value" using the config.json file.
+        """
+
+        with open(self.filepath) as json_file:
             return json.load(json_file)
 
-    '''
-        This function is used for generating a default config file.
-    '''
     def create_default_file(self):
+        """
+        This function is used for generating a default config file.
+        """
         # TODO: Finish
 
         default_config = {
@@ -42,19 +47,20 @@ class Config:
 
         self._write_json(default_config)
 
-    '''
+    def check_config_file(self):
+        """
         Checks the conformity of the config file. If detects that the config
         file is malformed or damaged (ex. non accessible, ...) this function
         will generate a new one using the 'create_default_file' function.
-    '''
-    def check_config_file(self):
+        """
         # TODO: Finish
 
         data = self._read_json()
         print("Check if there are all the required settings")
 
-    '''
-        Returns all the settings saved in the config file.
-    '''
     def get_settings(self):
+        """
+        Returns all the settings saved in the config file.
+        """
+
         return self._read_json()
